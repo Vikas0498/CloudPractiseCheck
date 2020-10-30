426c2fe1afd5554013e097ff317fed642cd38511
Vikas0498 <pandeyvikas0498@gmail.com>
2020-10-30 13:15:37 +0530

    first commit

---
+++ Controllers/MenuItemController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiDockerProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<MenuItem> Get()
+        {
+            return new List<MenuItem> {new MenuItem()
+                {
+                    Id = 1,
+                    Name = "Chicken",
+                    Price = 150,
+                    Active = true,
+                    DateOfLaunch = Convert.ToDateTime("2002-12-12 12:12:00.0000000"),
+                    FreeDelivery = true
+                },
+            new MenuItem()
+                {
+                    Id = 2,
+                    Name = "Tomato Soup",
+                    Price = 50,
+                    Active = true,
+                    DateOfLaunch = Convert.ToDateTime("2002-12-12 12:12:00.0000000"),
+                    FreeDelivery = true
+                }};
+        }
+    }
+}
---
+++ MenuItem.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApiDockerProject
+{
+    public class MenuItem
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public int Price { get; set; }
+        [Required]
+        public bool Active { get; set; }
+        [Required]
+        public DateTime DateOfLaunch { get; set; }
+        public bool FreeDelivery { get; set; }
+    }
+}
